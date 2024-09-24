bb7028c20e2e04ee96ede311639a139c7fb75ecc
acyberSlash <awrotslavsky@gmail.com>
2024-09-24 20:55:50 +0000

    saveee

--- lessons/00_Turtles/10c_Tash_Me_Twirl.py
+++ lessons/00_Turtles/10c_Tash_Me_Twirl.py
@@ -8,4 +8,32 @@ Hint: See 08a_More Turtle Programs, section 'Click on the Turtle'
 """
 
 ... # Your code here
+import turtle as turtle
 
+turtle.setup(width=600, height=600)
+
+def turtle_clicked(t, x, y):
+    """Function that gets called when the user clicks on the turtle
+
+    This function will make the turtle tilt 20 degrees 18 times, making a full
+    circle. It is called by the turtle when the user clicks on it.
+
+    Args:
+        t (Turtle): The turtle object that was clicked
+        x (int): The x coordinate of the click
+        y (int): The y coordinate of the click
+    """
+
+    print('turtle clicked!')
+
+
+
+t = turtle.Turtle()
+
+    
+for i in range(0,360, 20): # Full circle, 20 degrees at a time
+    t.tilt(20) # Tilt the turtle 20 degrees
+
+# Connect the turtle to the turtle_clicked function
+t.onclick(lambda x, y, t=t: turtle_clicked(t, x, y))
+turtle.done()
